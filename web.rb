ba9f28cf6ad92e386da96b4c34ea1394265f1247
Jack Callister <jarsbe@gmail.com>
2012-09-28 20:53:21 +1200

    Added lots of stuff.

--- web.rb
+++ web.rb
@@ -3,15 +3,15 @@ require 'faraday'
 require 'faraday_middleware'
 require 'json'
 
-get '/' do
-  connection = Faraday.new(:url => 'http://api.ihackernews.com') do |build|
+get "/" do
+  connection = Faraday.new(:url => "http://api.ihackernews.com") do |build|
     build.request  :json             
     build.response :json              
     build.adapter  Faraday.default_adapter
     build.use FaradayMiddleware::Mashify
   end
 
-  response = connection.get '/page'
+  response = connection.get "/page"
 
   @articles = Hashie::Mash.new(response.body).items
 
